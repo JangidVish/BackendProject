52120b953997b92cc4235c921e1955c29dd0afe5
Vishal Jangid <vishal.jangid_it22@pccoer.in>
2024-04-14 16:39:34 +0530

    Cloudinerry And multer middlewear

---
+++ src/utils/cloudmery.js
@@ -0,0 +1,31 @@
+import {v2 as cloudinary} from 'cloudinary'
+import fs from "fs"
+import { isNull } from 'util';
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUDNAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadOnCloudinary = async (localFilePath)=>{
+    try{
+        if(!localFilePath) return null
+
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        //File has been uploaded successfully
+        console.log("File is uploaded on cloudinary ", response.url);
+        return response
+
+    }
+    catch(error){
+        fs.unlinkSync(localFilePath) //remove the locally saved temporary file as the upload operation got failed
+
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
